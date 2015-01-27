3944893fe6b2bd4eac2333340b374071221b8f06
sojisub <597629876@qq.com>
2015-01-27 23:07:49 +0800

    周二晚上提交 获取pk列表话题

--- Application/Home/Controller/PkController.class.php
+++ Application/Home/Controller/PkController.class.php
@@ -2,12 +2,19 @@
 namespace Home\Controller;
 use Think\Controller;
 class PkController extends Controller {
+    private $_instance;
+    public function __construct() {
+        parent::__construct();
+        $this->_instance = D('Pk');
+    }
     public function pk_home(){
-       $objpk = D('Pk');
-       //$arr = $objpk->get_pk();
+       $arr = $this->_instance->get_pk();
        $this->display();
     }
     public function pk_list(){
+       $data = $this->_instance->pk_list();
+       $this->assign('show',$data['show']);
+       $this->assign('list',$data['list']);
        $this->display();
     }
     public function pk_comment(){
--- Application/Home/Model/PkModel.class.php
+++ Application/Home/Model/PkModel.class.php
@@ -2,15 +2,23 @@
 namespace Home\Model;
 use Think\Model;
 class PkModel extends Model{
-	/**
+      /**
 	*  取得PK言论
 	*/
-	public function get_pk()
-	{
-	    static $pkarr = array();
-	    
+	public function get_pk(){
 	    $pkarr = $this->select();
 	    printr($pkarr);
-		return $field ? $pkarr[$field] : $pkarr;
+            return $field ? $pkarr[$field] : $pkarr;
 	}
+        /*
+         * 取得pk话题列表
+         */
+        public function pk_list(){
+            $count = $this->count();// 查询满足要求的总记录数
+            $page = new \Think\Page($count,6);
+            $show = $page->show();
+            $list = $this->order('pubdate')->limit($page->firstRow.','.$page->listRows)->select();
+            $data = array('show'=>$show,'list'=>$list);
+            return $data;
+        }
 }
